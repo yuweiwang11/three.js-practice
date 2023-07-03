78823f3d7568914dd4fd95c65ddcf34dd367bcb6
Yuwei Wang <w67238@outlook.com>
2023-07-03 13:41:23 +1200

    add cube

---
+++ 03-basic-scene/script.js
@@ -0,0 +1,31 @@
+console.log('helo bruno')
+const scene = new THREE.Scene()
+
+// Red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// size
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 4
+camera.position.x = 1.2
+camera.position.y = 1
+
+scene.add(camera)
+
+//renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
